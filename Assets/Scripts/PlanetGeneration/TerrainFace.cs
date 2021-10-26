b0a7227ee0637694e887bf4275dedae72a4d1956
Marcin Zalewski <marcinzal24@gmail.com>
2021-10-26 10:29:59 +0200

    Added comments to TerrainFace.cs

--- Assets/Scripts/PlanetGeneration/TerrainFace.cs
+++ Assets/Scripts/PlanetGeneration/TerrainFace.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 
+// Summary:
+// Each planet is made out of 6 terrain faces, the initial sphere is made out of a cube
+// , that's why when the resolution is low, the sphere becomes a cube.
+// This technique is called quadrilateralized spherical cube.
+
 public class TerrainFace
 {
     Mesh mesh;  // UV.x = used for biomes, UV.y used for oceans
     ShapeGenerator shapeGenerator;
     int resolution;
 
+    // Axes used for claculating point on cube
     Vector3 localUp;
     Vector3 axisA;
     Vector3 axisB;
@@ -26,23 +32,29 @@ public class TerrainFace
         Vector3[] vertices = new Vector3[resolution * resolution];
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
         int triIndex = 0;
+        // We don't want to lose UV data when updating the mesh, so save it and assign it at the end
         Vector2[] uv = (mesh.uv.Length == vertices.Length) ? mesh.uv : new Vector2[vertices.Length];
 
         for(int y = 0; y < resolution; y++)
         {
             for(int x = 0; x < resolution; x++)
             {
+                // Index for setting vertices and uv's
                 int i = x + y * resolution;
 
                 // Percent tells you how complete the for loops are
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
 
+                // Calculate point on cube, then normalize it to get the point on a sphere shape
                 Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
                 Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+
+                // Calculate elevation using noise layers for this point on sphere
                 float unscaledElevation = shapeGenerator.CalculateUnscaledElevation(pointOnUnitSphere);
                 vertices[i] = pointOnUnitSphere * shapeGenerator.GetScaledElevation(unscaledElevation);
                 uv[i].y = unscaledElevation;
 
+                // Create the triangle aslong as the vertex is not along the right or bottom edge
                 if(x != resolution - 1 && y != resolution - 1)
                 {
                     triangles[triIndex] = i;
@@ -64,6 +76,7 @@ public class TerrainFace
         mesh.uv = uv;
     }
 
+    // Updating biomes is seperate from updating the mesh, because updating the mesh can be slow
     public void UpdateUVs(ColorGenerator colorGenerator)
     {
         Vector2[] uv = mesh.uv;
