7746f552c9d0180e20c72af54bc1819ca0bf6b19
Marcin Zalewski <marcinzal24@gmail.com>
2021-10-29 15:56:24 +0200

    Portfolio preview setup

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    public Vector3 rotateSpeed;
+
+    void Update()
+    {
+        transform.Rotate(rotateSpeed * Time.deltaTime);
+    }
+}
